87f7e0f517cee6914805d493a5f1805c79169791
Nandaraj-dvps <ujaradnan@gmail.com>
2026-06-16 02:58:50 +0100

    Shell practice

--- install-functions.sh
+++ install-functions.sh
@@ -8,27 +8,27 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-# # first arg -> what are you trying to install
-# # second arg -> exit code
-# VALIDATE(){
-#     if [ $2 -ne 0 ]; then
-#         echo "Installing $1 is ... FAILED"
-#         exit 1
-#     else
-#         echo "Installing $1 is ... SUCCESS"
-#     fi
-# }
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+}
 
-# # echo "I am continuing..."
-# dnf list installed mysql
+# echo "I am continuing..."
+dnf list installed mysql
 
-# if [ $? -eq 0 ]; then
-#     echo "MySQL is already installed ... SKIPPING"
-# else
-#     echo "Installing MySQL"
-#     dnf install mysql -y
-#     VALIDATE MySQL $?
-# fi
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed ... SKIPPING"
+else
+    echo "Installing MySQL"
+    dnf install mysql -y
+    VALIDATE MySQL $?
+fi
 
 # dnf list installed nginx
 # if [ $? -eq 0 ]; then
